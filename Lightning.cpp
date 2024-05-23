146b3783c44a237576856e2f12b07376299d751d
franek <franek_koziol@wp.pl>
2024-05-23 16:29:35 +0200

    removed unnecessary file

--- Lightning.cpp
+++
@@ -1,9 +0,0 @@
-#include <glm/glm.hpp>
-
-void Deffered(glm::vec2 TexCoords, glm::vec3 FragPos, glm::vec3 Normal){
-    glm::vec3 gPosition = FragPos;
-    glm::vec3 gNormal = normalize(Normal);
-    glm::vec4 gAlbedoSpec;
-    gAlbedoSpec.rgb = texture(texture_diffuse1, TexCoords).rgb;
-    gAlbedoSpec.a = texture(texture_specular1, TexCoords).r;
-}
